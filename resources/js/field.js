df610a97414c10c6123730101ac9720d12f3a429
Javier Martinez <fjmartinez@disitec.es>
2018-09-05 18:39:54 +0200

    default to empty object when value is null

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,5 +1,4 @@
 Nova.booting((Vue, router) => {
-  Vue.config.devtools = true;
   Vue.component('index-nova-json', require('./components/IndexField'));
   Vue.component('detail-nova-json', require('./components/DetailField'));
   Vue.component('form-nova-json', require('./components/FormField'));
